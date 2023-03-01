d650e4f6ebb1f114e9371c058f5383ac68ec0a35
James Luke Pendleton <99436608+jp-industrial@users.noreply.github.com>
2023-03-01 01:03:20 +0000

    Add files via upload

---
+++ control flow operators.py
@@ -0,0 +1,12 @@
+is_mage = False
+is_expert = True
+
+if is_mage and is_expert:
+    print("You are a master mage!")
+elif is_mage and not(is_expert):
+    print("Getting there!")
+elif not(is_mage):
+    print("You need magic powers!")
+    
+if 1 is 1:
+    print("the memory location of 1 and 1 is the same.
